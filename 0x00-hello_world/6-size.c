5710aa51671d370717e932a0640a439760a05ba5
mobkimark <mobkimarkus7@gmail.com>
2022-10-13 10:43:06 -0700

    Corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,13 +5,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-printf ("Size of a char: %zu byte(s) \n", size of(char));
-printf ("Size of int: %zu byte(s) \n", size of(int));
-printf ("Size of long int: %zu byte(s) \n", size of(long int));
-printf ("Size of long long int: %zu byte(s) \n", size of(long long int));
-printf ("Size of a float: %zu byte(s) \n", size of(float));
+printf("Size of a char: %zu byte(s) \n", sizeof(char));
+printf("Size of int: %zu byte(s) \n", sizeof(int));
+printf("Size of long int: %zu byte(s) \n", sizeof(long int));
+printf("Size of long long int: %zu byte(s) \n", sizeof(long long int));
+printf("Size of a float: %zu byte(s) \n", sizeof(float));
 
-return (0);
+return(0);
 }
