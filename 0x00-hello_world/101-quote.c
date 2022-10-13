946ca93ae16f3517cbaba95c282589f2a656ee9c
mobkimark <mobkimarkus7@gmail.com>
2022-10-13 12:26:16 -0700

    this is my last correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
 * main - Prints a text
 *
 * Return: Always 1 (Success)
 */
+
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 
